15506e9a5e310de6efeb57704982c0c773e89a35
HyunGyu-Lee <gusrb0808@naver.com>
2016-05-29 04:10:49 +0900

    First Upload

---
+++ DIMS/src/clients/Tester.java
@@ -0,0 +1,17 @@
+package clients;
+
+import javafx.application.Application;
+
+public class Tester {
+	
+	public static void main(String[] args) {
+		// 유아이수정ㄴㅁㅇ
+		Application.launch(MainApplication.class);
+		
+	}
+
+	
+		
+	
+	
+}
